365cc9d177f11aa186d7b7bb49c12296b43141fe
safiradewirahmatika <111473543+safiradewirahmatika@users.noreply.github.com>
2022-08-26 11:13:04 +0700

    Add files via upload

---
+++ 02-02/4.8. More on Defining Functions/4.8.1. Default Argument Values/index.py
@@ -0,0 +1,34 @@
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while Ture:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return Ture
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+i = 5
+def f(arg=i):
+    print(arg)
+i = 6
+f()
+
+def f(a, L=[]):
+    L.append(a)
+    return L
+print(f(1))
+print(f(2))
+print(f(3))
+
+def f(a, L=None):
+    if L is None:
+        L = []
+    L.append(a)
+    return L
+
+    
+
+        
