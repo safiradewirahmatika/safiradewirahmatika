32280183bffe1a54fe0916df8b1ce6067aeccbd8
safiradewirahmatika <111473543+safiradewirahmatika@users.noreply.github.com>
2022-08-26 11:10:09 +0700

    Add files via upload

---
+++ 02-05/7.1. Fancier Output Formatting/index.py
@@ -0,0 +1,23 @@
+year = 2016
+event = 'Referendum'
+print(f'Results of the {year} {event}')
+
+yes_votes = 42_572_654
+no_votes = 43_132_495
+percentage = yes_votes / (yes_votes + no_votes)
+print('{:-9} YES votes  {:2.2%}'.format(yes_votes, percentage))
+
+
+s = 'Hello, world.'
+str(s)
+repr(s)
+str(1/7)
+'0.14285714285714285'
+x = 10 * 3.25
+y = 200 * 200
+s = 'The value of x is ' + repr(x) + ', and y is ' + repr(y) + '...'
+print(s)
+hello = 'hello, world\n'
+hellos = repr(hello)
+print(hellos)
+repr((x, y, ('spam', 'eggs')))
